5b590d68d98fd35afdb17d87f821a84982fa2e35
Kosuke Kito <blueberryjam09@gmail.com>
2020-01-31 13:17:45 +0900

    Add abc086a for cpp

---
+++ cpp/beginners_selection/abc086a.cpp
@@ -0,0 +1,11 @@
+// https://atcoder.jp/contests/abs/tasks/abc086_a
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    string s = (a*b % 2 == 0) ? "Even" : "Odd";
+    cout << s << endl;
+}

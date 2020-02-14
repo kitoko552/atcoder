afb8ce094fe27ca8b8a6fc76f66923f3d4f77dde
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-14 19:22:59 +0900

    Fix problem E of 153

--- cpp/beginner_contest_153/e.cpp
+++ cpp/beginner_contest_153/e.cpp
@@ -2,10 +2,11 @@
 
 #include<iostream>
 #include<cmath>
-
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
-
-typedef pair<int, int> P;
+using ll = long long;
+using P = pair<int, int>;
+const int MAX_NUM = 100000000;
 
 int main() {
     int H, N;
@@ -16,82 +17,20 @@ int main() {
         cin >> A[i] >> B[i];
     }
 
-    int maxA = 0;
-    for (int i = 0; i < N - 1; i++) {
-        maxA = max(A[i], A[i+1]);
+    int maxa = 0;
+    rep(i,N) maxa = max(maxa, A[i]);
+
+    int dp[H+maxa+1];
+    rep(i,H+maxa+1) dp[i] = MAX_NUM;
+
+    rep(i,H+maxa+1)rep(j,N) {
+        if (A[j] >= i) dp[i] = min(dp[i], B[j]);
+        else dp[i] = min(dp[i], dp[i - A[j]] + B[j]);
     }
 
-    for (int i = H; i < H + maxA; i++) {
-        
-    }
+    int ans = MAX_NUM;
+    for (int i = H; i < H + maxa + 1; i++) ans = min(ans, dp[i]);
 
-    int ump = 0;
-    while (H > 0) {
-        // 体力Hを減らすための最小の組み合わせ
-        int pow;
-        int mp;
-        int cost = 10000;
-        // float perf = 0.0f;
-        for (int i = 0; i < N; i++) {
-            int cos = ceil(float(H) / float(A[i])) * B[i];
-            // cout << i << ":" << cos << endl;
-            if (cos < cost) {
-                cost = cos;
-                pow = A[i]; 
-                mp = B[i];
-            }
-        }
-        H -= pow;
-        ump += mp;
-    }
-
-    
-    // // 1魔力あたりの攻撃力が最も大きいものを使って貪欲法
-    // int pow;
-    // int mp;
-    // float perf = 0.0f;
-    // for (int i = 0; i < N; i++) {
-    //     float cost = float(A[i]) / float(B[i]);
-    //     cout << i << ":" << cost << endl;
-    //     if (perf < cost) {
-    //         perf = cost;
-    //         pow = A[i]; 
-    //         mp = B[i];
-    //     }
-    // }
-
-    // int ump = 0;
-    // while (H >= pow) {
-    //     H -= pow;
-    //     ump += mp;
-    // }
-
-    // if (H > 0) {
-    //     // 1発で殺せるかつ魔力が最小の魔法で殺す
-    //     P p[N];
-    //     for (int i = 0; i < N; i++) {
-    //         p[i] = P(A[i], B[i]);
-    //         // cout << p[i].first << " " << p[i].second << endl;
-    //     }
-
-    //     // 魔力で昇順ソート
-    //     sort(p, p+N, [](P x, P y) { return x.second < y.second; });
-
-    //     // for (int i = 0; i < N; i++) {
-    //     //     cout << p[i].first << ", " << p[i].second << endl;
-    //     // }
-
-    //     // 残りHPに対して最もMP消費が少ない攻撃パターンで殺す
-    //     for (int i = 0; i < N; i++) {
-    //         if (H <= p[i].first) {
-    //             ump += p[i].second;
-    //             break;
-    //         }
-    //     }
-    // }
-
-    cout << H << endl;
-    cout << ump << endl;
-    
+    cout << ans << endl;    
     return 0;
 }

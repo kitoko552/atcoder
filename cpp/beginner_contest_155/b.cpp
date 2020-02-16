67b9e784db9df1cd6cb3ae294fc25b19dac535ec
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-17 00:15:49 +0900

    Fix b

--- cpp/beginner_contest_155/b.cpp
+++ cpp/beginner_contest_155/b.cpp
@@ -2,6 +2,7 @@
 
 #include<iostream>
 #include<cmath>
+#include<vector>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define rep1(i, n) for (int i = 1; i < (n); ++i)
 using namespace std;
@@ -11,15 +12,15 @@ using P = pair<int, int>;
 int main() {
     int N;
     cin >> N;
-    int A[N+1];
+    int A[N];
     rep(i,N) cin >> A[i];
 
     string ans = "APPROVED";
     rep(i,N) {
-        if (A[i] % 2 == 0) 
-            if (A[i] % 3 != 0 && A[i] % 5 != 0)
-                ans = "DENIED";
-                break;
+        if (A[i] % 2 == 0 && A[i] % 3 != 0 && A[i] % 5 != 0) {
+            ans = "DENIED";
+            break;
+        }
     }
 
     cout << ans << endl;

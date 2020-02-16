4d4452f93dec733fa1e7a063891ab2ab3113483b
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-16 17:08:31 +0900

    Update c

--- cpp/educational_dp_contest/c.cpp
+++ cpp/educational_dp_contest/c.cpp
@@ -4,7 +4,6 @@
 #include<cmath>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define rep1(i, n) for (int i = 1; i < (n); ++i)
-#define rep1e(i, n) for (int i = 1; i <= (n); ++i)
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
@@ -15,23 +14,23 @@ int main() {
     int a[N], b[N], c[N];
     rep(i,N) cin >> a[i] >> b[i] >> c[i];
 
-    int dp[N][3];
-    rep(i,N)rep(j,3) dp[i][j] = 0;
+    int dp[N+1][3];
+    rep(i,N+1)rep(j,3) dp[i][j] = 0;
     dp[0][0] = a[0];
     dp[0][1] = b[0];
     dp[0][2] = c[0];
 
     // dp[i][j] = 太郎くんがi日目に行動jをとったとき得る幸福度の総和の最大値
-    rep1(i,N)rep(j,3) {
+    rep(i,N+1)rep(j,3) {
         if (j == 0)
-            dp[i][j] = a[i] + max(dp[i-1][1], dp[i-1][2]);
+            dp[i+1][j] = a[i+1] + max(dp[i][1], dp[i][2]);
         if (j == 1)
-            dp[i][j] = b[i] + max(dp[i-1][0], dp[i-1][2]);
+            dp[i+1][j] = b[i+1] + max(dp[i][0], dp[i][2]);
         if (j == 2)
-            dp[i][j] = c[i] + max(dp[i-1][0], dp[i-1][1]);
+            dp[i+1][j] = c[i+1] + max(dp[i][0], dp[i][1]);
     }
 
-    int ans = max(dp[N-1][0], max(dp[N-1][1], dp[N-1][2]));
+    int ans = max(dp[N][0], max(dp[N][1], dp[N][2]));
     cout << ans << endl;
     return 0;
 }

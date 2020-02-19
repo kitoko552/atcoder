f05675578998c7b4ffe34699d6077f67e002bb1d
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-19 16:15:41 +0900

    Fix E

--- cpp/begginer_contest_152/e.cpp
+++ cpp/begginer_contest_152/e.cpp
@@ -11,45 +11,138 @@ using ll = long long;
 using P = pair<int, int>;
 const int INF = 1001001001;
 
-ll lcm(ll a, ll b) {
-  ll x = a * b;
+// エラトステネスの篩（Sieve of Erastosthenes）
+struct Sieve {
+  int n;
 
-  // 自然数 a > b を確認・入替
-  if(a<b){
-    ll tmp = a;
-    a = b;
-    b = tmp;
+  // 0からnまで、その数を素因数分解した時に求まる最小の素数のリスト
+  // 外部から使われることはないはず
+  vector<int> f;
+
+  // nまでの素数リスト
+  vector<int> primes;
+
+  Sieve(int n=1):n(n), f(n+1) {
+    f[0] = f[1] = -1; // 0と1は素数ではないので例外的に-1を入れる
+    for (ll i = 2; i <= n; ++i) {
+      if (f[i]) continue;
+      primes.push_back(i);
+      f[i] = i;
+      for (ll j = i*i; j <= n; j += i) {
+        if (!f[j]) f[j] = i;
+      }
+    }
   }
 
-  // ユークリッドの互除法
-  ll r = a % b;
-  while(r!=0){
-    a = b;
-    b = r;
-    r = a % b;
+  // xが素数かどうかのbool値を返す (x <= n)
+  bool isPrime(int x) { return f[x] == x;}
+
+  // xの素因数分解の結果リストを返す (x <= n)
+  vector<int> factorList(int x) {
+    vector<int> res;
+    while (x != 1) {
+      res.push_back(f[x]);
+      x /= f[x];
+    }
+    return res;
   }
 
-  // 最小公倍数を出力
-  return x/b;
-}
+  // xの素因数分解の結果をP(基数, 指数)の形にしたリストを返す (x <= n)
+  vector<P> factor(int x) {
+    vector<int> fl = factorList(x);
+    if (fl.size() == 0) return {};
+    vector<P> res(1, P(fl[0], 0));
+    for (int p : fl) {
+      if (res.back().first == p) {
+        res.back().second++;
+      } else {
+        res.emplace_back(p, 1);
+      }
+    }
+    return res;
+  }
+};
+
+const int mod = 1000000007;
+
+struct mint {
+  ll x;
+
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+ 
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
 
 int main() {
-  int N;
-  cin >> N;
-  ll A[N];
-  rep(i,N) cin >> A[i];
-
-  ll alcm = 1;
-  rep(i,N) {
-    alcm = lcm(alcm, A[i]);
+  Sieve sieve(1e6);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i,n) cin >> a[i];
+ 
+  map<int,int> mp;
+  rep(i,n) {
+    auto f = sieve.factor(a[i]);
+    for (auto p : f) {
+      mp[p.first] = max(mp[p.first], p.second);
+    }
   }
-
-  ll sum = 0;
-  rep(i,N) {
-    sum += alcm/A[i];
+ 
+  mint lcm = 1;
+  for (auto p : mp) {
+    rep(i,p.second) {
+      lcm *= p.first;
+    }
   }
-
-  ll ans = sum % (1000000000 + 7);  
-  cout << ans << endl;
+ 
+  mint ans;
+  rep(i,n) {
+    mint b = lcm/a[i];
+    ans += b;
+  }
+  cout << ans.x << endl;
   return 0;
 }

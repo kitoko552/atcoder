a13a2c8b73e72011d460d80f4a1df02e4255221a
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-19 16:51:27 +0900

    Add a of BC151

---
+++ cpp/beginner_contest_151/a.cpp
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc151/tasks/abc151_a
+
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<map>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = 1001001001;
+
+int main() {
+  char C;
+  cin >> C;
+
+  char ans = C+1;
+  cout << ans << endl;
+  return 0;
+}

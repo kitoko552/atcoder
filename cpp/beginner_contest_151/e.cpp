365a119afbd6b576faa01fe325a8211f9cf8acb4
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-20 16:11:50 +0900

    Add e of BC151

--- cpp/beginner_contest_151/e.cpp
+++ cpp/beginner_contest_151/e.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include <algorithm>
 #include<cmath>
 #include<vector>
 #include<map>
+#include<queue>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define rep1(i, n) for (int i = 1; i < (n); ++i)
 using namespace std;
@@ -64,7 +66,6 @@ struct mint {
 struct combination {
   vector<mint> fact, ifact;
   combination(int n):fact(n+1),ifact(n+1) {
-    assert(n < mod);
     fact[0] = 1;
     for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
     ifact[n] = fact[n].inv();
@@ -79,8 +80,28 @@ struct combination {
 int main() {
   int N, K;
   cin >> N >> K;
+  vector<ll> A(N);
+  rep(i,N) cin >> A[i];
 
-  combination comb(1000000);
-  cout << comb(N, K).x << endl;
+  combination com(100001);
+
+  sort(A.begin(), A.end());
+  mint mxsum = 0;
+  rep(i,N) {
+    if(i < K-1) continue;
+    mint a = A[i];
+    mxsum += com(i,K-1) * a;
+  }
+
+  sort(A.begin(), A.end(), greater<>());
+  mint mnsum = 0;
+  rep(i,N) {
+    if(i < K-1) continue;
+    mint a = A[i];
+    mnsum += com(i,K-1) * a;
+  }
+
+  mint ans = mxsum - mnsum;
+  cout << ans.x << endl;
   return 0;
 }

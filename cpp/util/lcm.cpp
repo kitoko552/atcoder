a7bf7b9a4e3ac755bed80797dc6712a758646c80
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-21 16:24:46 +0900

    Update lcm

--- cpp/util/lcm.cpp
+++ cpp/util/lcm.cpp
@@ -9,25 +9,28 @@ using ll = long long;
 using P = pair<int, int>;
 const int INF = 1001001001;
 
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+
 // aとbの最小公倍数を求める
-ll lcm(ll a, ll b) {
-  ll x = a * b;
+// ll lcm(ll a, ll b) {
+//   ll x = a * b;
 
-  // 自然数 a > b を確認・入替
-  if(a<b){
-    ll tmp = a;
-    a = b;
-    b = tmp;
-  }
+//   // 自然数 a > b を確認・入替
+//   if(a<b){
+//     ll tmp = a;
+//     a = b;
+//     b = tmp;
+//   }
 
-  // ユークリッドの互除法
-  ll r = a % b;
-  while(r!=0){
-    a = b;
-    b = r;
-    r = a % b;
-  }
+//   // ユークリッドの互除法
+//   ll r = a % b;
+//   while(r!=0){
+//     a = b;
+//     b = r;
+//     r = a % b;
+//   }
 
-  // 最小公倍数を出力
-  return x/b;
-}
+//   // 最小公倍数を出力
+//   return x/b;
+// }

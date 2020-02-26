5865c441519f860b17a3b643fc5e746aa0265b69
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-26 14:53:54 +0900

    Add B of BC148

---
+++ cpp/beginner_contest_148/a.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<map>
+#include<queue>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = 1001001001;
+
+int main() {
+  int A, B;
+  cin >> A;
+  cin >> B;
+
+  cout << 6-A-B << endl;
+  return 0;
+}

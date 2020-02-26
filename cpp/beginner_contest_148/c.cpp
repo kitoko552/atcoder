7f27ce113bcebff825ab66eafba5e9917eb2588c
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-26 15:00:14 +0900

    Add C of BC148

---
+++ cpp/beginner_contest_148/c.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<map>
+#include<queue>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = 1001001001;
+
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  cout << lcm(A,B) << endl;
+  return 0;
+}

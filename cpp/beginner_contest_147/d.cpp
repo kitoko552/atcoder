a5271317d2e719bdcb7ffa221825a7c1bda78955
Kosuke Kito <blueberryjam09@gmail.com>
2020-02-28 12:26:01 +0900

    Add D of BC147

---
+++ cpp/beginner_contest_147/d.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<map>
+#include<queue>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = 1001001001;
+
+const int mod = 1000000007;
+struct mint {
+  ll x;
+
+  mint(ll x=0):x((x%mod+mod)%mod){}
+
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+ 
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+
+int main() {
+  int N;
+  cin >> N;
+  ll A[N];
+  rep(i,N) cin >> A[i];
+
+  mint ans = 0;
+  rep(k,60) {
+    mint n1 = 0;
+    rep(i,N) {
+      if((A[i] >> k) & 1) n1+=1;
+    }
+    ans += n1*(mint(N)-n1)*mint(2).pow(k);
+  }
+  cout << ans.x << endl;
+  return 0;
+}

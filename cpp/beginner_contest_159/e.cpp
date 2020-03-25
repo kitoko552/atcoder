c76a22940123271a78056405ebfc42d2a1bc3726
Kosuke Kito <blueberryjam09@gmail.com>
2020-03-25 20:27:41 +0900

    Add comment

--- cpp/beginner_contest_159/e.cpp
+++ cpp/beginner_contest_159/e.cpp
@@ -13,17 +13,42 @@ using ll = long long;
 using P = pair<int, int>;
 const int INF = 1001001001;
 
-map<int,int> sum;
+int H, W, K;
+// string S[10];
+char S[10][1000];
+// int S[10][1000];
+
+int cut_vertical(int top, int bottom) {
+  int ans = 0;
+  int numw = 0;
+  int w = 0;
+  while(w<W) {
+    for(int h=top; h<bottom; h++) {
+      int n = S[h][w]-'0';
+      if(n == 1) numw++;
+    }
+    if(numw >= K) {
+      while(numw >= K) {
+        ans++;
+        numw -= K;
+      }
+    }
+    w++;
+  }
+  return ans;
+}
 
 int main() {
-  int H, W, K;
   cin >> H >> W >> K;
-  int S[H][W];
-  rep(i,H)rep(j,W) {
-    cin >> S[i][j];
-    if (S[i][j] == 1) sum[i] += 1;
-  }
-  
+  rep(i,H)rep(j,W) cin >> S[i][j];  
+
+  // int ans = cut_vertical(0,H);
+  // cout << ans << endl;
+
+  int ans = 0;
+  // 横の割り方全通りに対して縦の割り方を貪欲法で求める
+  // どうやって横の割り方全通りを表現するか
+
   return 0;
 }
 

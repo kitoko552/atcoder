8d8f87893448cb6d079f2cce4ff1b1673f656150
kitoko552 <blueberryjam09@gmail.com>
2020-03-29 09:24:21 +0900

    Add D of ABC160

--- cpp/beginner_contest_160/d.cpp
+++ cpp/beginner_contest_160/d.cpp
@@ -11,12 +11,28 @@ using ll = long long;
 using P = pair<int, int>;
 const int INF = 1001001001;
 
-int main() {
-  int N, M;
-  cin >> N >> M;
+int ans[2*1000];
 
-  int ans = N*(N-1)/2 + M*(M-1)/2;
-  cout<< ans << endl;
+int main() {
+  int N, X, Y;
+  cin >> N >> X >> Y;
+
+  ans[0] = N;
+  int a_all = N-(Y-X-1)-1;
+  for(int i=2; i<=a_all; i++) {
+    ans[i-1] += a_all-i+1;
+  }
+
+  int b_all = N-(N-Y+1)-1;
+  for(int i=2; i<=b_all; i++) {
+    ans[i-1] += b_all-i+1;
+  }
+
+  int c_all = N-X-1;
+  for(int i=2; i<=c_all; i++) {
+    ans[i-1] += c_all-i+1;
+  }
+  rep(i,N-1) cout << ans[i] << endl;
   return 0;
 }
 
